51dba63e3633b0b6ad3b26ad33537018a8839696
Kevin Chien <kevinchien17@gmail.com>
2016-11-18 02:52:43 -0800

    cleanup

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 #Features
-Switch to branch simpleRecordingApp to find main loop recording app features.
-Switch to branch Kelly to find bpm tap interface.
+* Functional loop recording and playback. Press record to record, stop to stop recording/track, play to play recording/track.
+* Assistant module for intuitive bpm detection.
+
+Project was made with <3 during [Cal Hacks 3.0](https://devpost.com/software/loop-pedal).
 Switch to Lawrence's branch to find arduino code for loop pedal.
 
 #Development quickstart
 `git clone https://github.com/kev-chien/LoopPedal.git`
 `cd LoopPedal`
 
-Git guide:
-create your own branch (while switching): `git checkout -b "Your-name-here"`
+##Git guide:
+create your own branch (while switching): `git checkout -b your_branch_name`
 
-to save a change on local:
+###to save a change on local:
 `git add 'file changed'`
 `git commit -m 'Commit message here'`
-`git push origin master`
+
+###to push to github:
+`git push origin your_branch_name`
--- SimpleRecordingApp/ViewController.swift
+++ SimpleRecordingApp/ViewController.swift
@@ -26,11 +26,11 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
     
     //timer
     var time = 0
-    var timetimer = Timer()
-    var metronomeTimer = Timer()
-    var metronomeTime = 0
-    var metronomeActivated = 0
-    var count = 0
+    var intervalTimer = Timer()
+    //var metronomeTimer = Timer()
+    //var metronomeTime = 0
+    //var metronomeActivated = 0
+    var intervalCount = 0
     
     //save BPM
     var saveBPM1 = 0
@@ -43,6 +43,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
         // Dispose of any resources that can be recreated.
     }
     
+    var metAlpha = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -90,19 +91,19 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
     }
 
     @IBAction func bpmTap(_ sender: Any) {
-        timetimer.invalidate()
-        timetimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(ViewController.action), userInfo: nil, repeats: true)
-        count += 1
-        print(count)
+        intervalTimer.invalidate()
+        intervalTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(ViewController.action), userInfo: nil, repeats: true)
+        intervalCount += 1
+        print(intervalCount)
         
-        if(count==4){
+        if(intervalCount==4){
             saveBPM3 = time
             print("4", time)
-            timetimer.invalidate()
+            intervalTimer.invalidate()
             time = 0
             //timerLabel.text = ("Done")
             averageBPM = (saveBPM1+saveBPM2+saveBPM3)/3
-            count = 0
+            intervalCount = 0
             //AverageBPMLabel.text = "Play Beat"
             print("av", averageBPM)
             actualBPM = 6000 / averageBPM
@@ -110,19 +111,22 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
             bpmTapOutlet.setTitle("Tap to reset", for: UIControlState.normal)
             
             //activate metronome
+            /*
             if metronomeActivated == 0{
                 metronomeActivated = 1
-            }
+                metronomeTimer.invalidate()
+                metronomeTimer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(ViewController.action2), userInfo: nil, repeats: true)
+            }*/
         }
-        if(count==1){
+        if(intervalCount==1){
             bpmTapOutlet.setTitle("Beat 2", for: UIControlState.normal)
         }
-        if(count==2){
+        if(intervalCount==2){
             saveBPM1 = time
             print("2", time)
             bpmTapOutlet.setTitle("Beat 3", for: UIControlState.normal)
         }
-        if(count==3){
+        if(intervalCount==3){
             saveBPM2 = time
             print("3", time)
             bpmTapOutlet.setTitle("Beat 4", for: UIControlState.normal)
@@ -137,6 +141,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
         
         if audioRecorder?.isRecording == true {
             audioRecorder?.stop()
+            playAudioFunc()
         } else {
             audioPlayer?.stop()
         }
@@ -144,6 +149,10 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
     
     
     @IBAction func playAudio(_ sender: UIButton?) {
+        playAudioFunc()
+    }
+    
+    func playAudioFunc(){
         if audioRecorder?.isRecording == false {
             stopButton.isEnabled = true
             recordButton.isEnabled = false
@@ -161,6 +170,14 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
         }
     }
     
+    
+    func action2()
+    {
+        //time += 1
+        //metAlpha = CGFloat((cos(2.0 * 3.14 * time/ Double(averageBPM)) + 1.0)/2.0)
+        //metronomeLabel.alpha = metAlpha
+        //print(time)
+    }
     
     func action()
     {
